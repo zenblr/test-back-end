15bb31ce6c72ea069bdbb6c04aad405f852ca98c
Ravi Kumar <ravi.kumar@neebal.com>
2020-04-14 10:45:28 +0530

    upload

--- gold-admin-frontend/src/app/views/pages/upload-data/upload-offer/upload-offer.component.ts
+++ gold-admin-frontend/src/app/views/pages/upload-data/upload-offer/upload-offer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { UploadBannerService } from '../../../../core/upload-data/upload-banner/services/upload-banner.service';
 
 @Component({
@@ -8,6 +8,8 @@ import { UploadBannerService } from '../../../../core/upload-data/upload-banner/
 })
 export class UploadOfferComponent implements OnInit {
   images: any[] = []
+  index: number = null
+  @ViewChild("file", { static: false }) file;
 
   constructor(private uploadBannerService: UploadBannerService,
     private ref: ChangeDetectorRef) { }
@@ -15,33 +17,40 @@ export class UploadOfferComponent implements OnInit {
   ngOnInit() {
   }
 
-  addImages() {
-    var data =
-    {
-      value: '',
-      isRemoved: false,
-      isEditable: false,
-      showImg: false,
-    }
-    this.images.push(data)
+
+  uploadImages(event) {
+
+    var reader = new FileReader()
     console.log(this.images)
+    if (event.target.files.length == 0) {
+      this.index == null
+    } else {
+      reader.readAsDataURL(event.target.files[0]);
+      var fd = new FormData()
+      fd.append('avatar', event.target.files[0])
+      this.uploadBannerService.uploadFile(fd).subscribe(
+        res => {
+          console.log(reader.result);
+          if (this.index != null) {
+            this.images.splice(this.index, 1, res.uploadFile.URL)
+            this.index = null;
+          } else {
+            this.images.push(res.uploadFile.URL)
+          }
+          console.log(this.images)
+          this.ref.detectChanges();
+
+        })
+    }
   }
 
-  uploadImages(event, index: number) {
-    var reader = new FileReader()
-    this.images[index].value = event.target.files[0]
-    this.images[index].isEditable = true;
-    this.images[index].isRemoved = true;
-    console.log(this.images[index])
-    reader.readAsDataURL(event.target.files[0]);
-    var fd = new FormData()
-    fd.append('avatar', this.images[index].value)
-    this.uploadBannerService.uploadFile(fd).subscribe(
-      res => {
-        console.log(reader.result);
-        this.images[index].value = res.uploadFile.URL
-        this.images[index].showImg = true;
-        this.ref.detectChanges()
-      })
+  delete(index: number) {
+    this.images.splice(index, 1)
+    this.ref.detectChanges();
+  }
+
+  upload(idx) {
+    this.file.nativeElement.click()
+    this.index = idx;
   }
 }
