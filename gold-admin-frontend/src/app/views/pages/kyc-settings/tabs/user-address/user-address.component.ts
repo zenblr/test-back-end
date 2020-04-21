1fd60a5063ff8bc633ff810735e3549e3f6863ee
Bhushan Madaye <bhushanmadaye@nimapinfotech.com>
2020-04-21 16:09:44 +0530

    user-address: UI

--- gold-admin-frontend/src/app/views/pages/kyc-settings/tabs/user-address/user-address.component.ts
+++ gold-admin-frontend/src/app/views/pages/kyc-settings/tabs/user-address/user-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'kt-user-address',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAddressComponent implements OnInit {
 
-  constructor() { }
+  identityForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    // this.identityForm = this.fb.group({
+    //   proofType: []
+    // });
+  }
+
+  getFileInfo(event) {
+    // this.file = event.target.files[0];
+    // var ext = event.target.files[0].name.split('.');
+    // if(ext[ext.length - 1] != 'csv' ){
+    //   this._toastr.error('Please upload csv file');
+    //   this.csvForm.controls.csv.markAsTouched()
+    //   return
+    // }
+    // this.csvForm.get('csv').patchValue(event.target.files[0].name);
+
   }
 
 }
