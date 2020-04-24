536081fc117a8d237a94db9c92bbe883941c84f8
Rupesh <rupesh@nimapinfotech.com>
2020-04-24 18:38:02 +0530

    add global search in lead customer api & gold rate

--- gold-loan-backend/utils/pagination.js
+++ gold-loan-backend/utils/pagination.js
@@ -4,7 +4,7 @@ const check = require('../lib/checkLib');
 let paginationWithFromTo = (searchParameter, fromParameter, toParameter) => {
     let search = check.isEmpty(searchParameter) ? "" : searchParameter;
     let from = check.isEmpty(fromParameter) ? 1 : fromParameter;
-    let to = check.isEmpty(toParameter) ? 10 : toParameter;
+    let to = check.isEmpty(toParameter) ? 25 : toParameter;
     let pageSize = Number((to - from) + 1);
     let offset = Number(from - 1);
     return { search, offset, pageSize };
@@ -13,7 +13,7 @@ let paginationWithFromTo = (searchParameter, fromParameter, toParameter) => {
 let paginationWithPageNumberPageSize = (searchParameter, pageNumberParameter, pageSizeParameter) => {
     let username = check.isEmpty(searchParameter) ? "" : searchParameter;
     let pageNumber = !check.isEmpty(pageNumberParameter) ? pageNumberParameter : 1;
-    let pageSize = pageSizeParameter || 10;
+    let pageSize = pageSizeParameter || 25;
     if (pageSize != 0 && pageNumber != 0) {
         userOffset = (pageSize * (pageNumber - 1));
     }
