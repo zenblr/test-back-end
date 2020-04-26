3fe1b52d9dada84732aa888b9149f38affab26a0
pranali95 <sonatan@nimapinfotech.com>
2020-04-26 21:32:28 +0530

    After login get permissions details added

--- gold-loan-backend/middleware/checkRolesPermissions.js
+++ gold-loan-backend/middleware/checkRolesPermissions.js
@@ -8,69 +8,69 @@ const redisClient = redis.createClient(redisConn.PORT, redisConn.HOST);
 module.exports = async (req, res, next) => {
     try {
         let userId = await req.userData.id;
-        let requestInfo={
-            'method':req.method,
-            'baseUrl':req.baseUrl,
-            'path':req.route.path
+        let requestInfo = {
+            'method': req.method,
+            'baseUrl': req.baseUrl,
+            'path': req.route.path
         }
         console.log(requestInfo);
         let systemInfo;
 
         await redisClient.get(`${userId}permissions`, async (err, result) => {
             if (err) {
-              res.send(err);
+                res.send(err);
             }
             else if (result) {
-              result = JSON.parse(result);
-              systemInfo =await  result;
-              if(systemInfo.length!=0){
-                let access = false
-               await systemInfo.forEach(data=>{
-                   if(data.method.toLowerCase() === requestInfo.method.toLowerCase() && data.baseUrl.toLowerCase() === requestInfo.baseUrl.toLowerCase() && data.path.toLowerCase() === requestInfo.path.toLowerCase()){
-                       access = true
-                   }  
-               })
-               if(access){
-                   next();
-               } else {
-                   res.status(403).json({message:'access denied'})
-               }
-               } else {
-                   res.status(403).json({message:'access denied'})
-               }
+                result = JSON.parse(result);
+                systemInfo = await result;
+                if (systemInfo.length != 0) {
+                    let access = false
+                    await systemInfo.forEach(data => {
+                        if (data.method.toLowerCase() === requestInfo.method.toLowerCase() && data.baseUrl.toLowerCase() === requestInfo.baseUrl.toLowerCase() && data.path.toLowerCase() === requestInfo.path.toLowerCase()) {
+                            access = true
+                        }
+                    })
+                    if (access) {
+                        next();
+                    } else {
+                        res.status(403).json({ message: 'access denied' })
+                    }
+                } else {
+                    res.status(403).json({ message: 'access denied' })
+                }
             } else {
-                let getRole = await  models.userRole.getAllRole(userId);
+                let getRole = await models.userRole.getAllRole(userId);
                 let roleId = await getRole.map((data) => data.roleId);
                 let getPermissions = await models.rolePermission.findAll({
-                    where : {roleId: {[Op.in]: roleId}},
+                    where: { roleId: { [Op.in]: roleId }, isActive: true },
                     attributes: ['permissionId']
-                    });
+                });
                 let permissionId = await getPermissions.map((data) => data.permissionId);
                 let systemInfoData = await models.permissionSystemInfo.findAll({
-                    where : {permissionId: {[Op.in]: permissionId}},
+                    where: { permissionId: { [Op.in]: permissionId }, isActive: true },
                     attributes: ['systemInfo']
-                    });
-                systemInfo =  await systemInfoData.map((data) => data.systemInfo);
-              redisClient.set(`${userId}permissions`, JSON.stringify(systemInfo));
-              const todayEnd = new Date().setHours(23, 59, 59, 999);
-              redisClient.expireat(`${userId}permissions`, parseInt(todayEnd / 1000));
-              if(systemInfo.length!=0){
-                let access = false
-               await systemInfo.forEach(data=>{
-                   if(data.method.toLowerCase() === requestInfo.method.toLowerCase() && data.baseUrl.toLowerCase() === requestInfo.baseUrl.toLowerCase() && data.path.toLowerCase() === requestInfo.path.toLowerCase()){
-                       access = true
-                   }  
-               })
-               if(access){
-                   next();
-               } else {
-                   res.status(403).json({message:'access denied'})
-               }
-               } else {
-                   res.status(403).json({message:'access denied'})
-               }
+                });
+                systemInfo = await systemInfoData.map((data) => data.systemInfo);
+                redisClient.set(`${userId}permissions`, JSON.stringify(systemInfo));
+                const todayEnd = new Date().setHours(23, 59, 59, 999);
+                redisClient.expireat(`${userId}permissions`, parseInt(todayEnd / 1000));
+                if (systemInfo.length != 0) {
+                    let access = false
+                    await systemInfo.forEach(data => {
+                        if (data.method.toLowerCase() === requestInfo.method.toLowerCase() && data.baseUrl.toLowerCase() === requestInfo.baseUrl.toLowerCase() && data.path.toLowerCase() === requestInfo.path.toLowerCase()) {
+                            access = true
+                        }
+                    })
+                    if (access) {
+                        next();
+                    } else {
+                        res.status(403).json({ message: 'access denied' })
+                    }
+                } else {
+                    res.status(403).json({ message: 'access denied' })
+                }
             }
-          })
+        })
     } catch (err) {
         res.status(401).send(err);
     }
