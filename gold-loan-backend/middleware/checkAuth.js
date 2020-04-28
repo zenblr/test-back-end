765626ee6f6e59e3d45287073b1872300503362d
Rupesh <rupesh@nimapinfotech.com>
2020-04-28 17:43:55 +0530

    changes in checkauth

--- gold-loan-backend/middleware/checkAuth.js
+++ gold-loan-backend/middleware/checkAuth.js
@@ -8,16 +8,13 @@ const client = redis.createClient(redisConn.PORT, redisConn.HOST);
 
 
 module.exports = async (req, res, next) => {
+    const token = await req.headers.authorization.split(" ")[1];
     try {
-        const token = await req.headers.authorization.split(" ")[1];
-
         const decoded = await jwt.verify(token, CONSTANT.JWT_SECRETKEY);
         req.userData = decoded;
         next();
     } catch (error) {
-        let token = await req.headers.authorization.split(" ")[1];
-        let abc = await models.logger.destroy({ where: { token: token } });
-        console.log(abc)
+        await models.logger.destroy({ where: { token: token } });
         client.del(token, JSON.stringify(token));
         return res.status(401).json({
             message: "auth failed"
