d9558e29bc55fa5f1139bf9eb1899e791a942ef5
Manish Chandwani <manish@nimapinfotech.com>
2020-04-29 14:23:45 +0530

    changes in scheme api

--- gold-loan-backend/controllers/scheme/scheme.js
+++ gold-loan-backend/controllers/scheme/scheme.js
@@ -1,9 +1,10 @@
 const models = require('../../models');
 const sequelize = models.sequelize;
+const check=require('../../lib/checkLib');
 
 // add scheme
 exports.addScheme = async (req, res, next) => {
-    const { schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
+    const { schemeName,schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
         interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually, partnerId } = req.body;
 
     if (schemeAmountStart >= schemeAmountEnd) {
@@ -11,8 +12,13 @@ exports.addScheme = async (req, res, next) => {
     }
 
     await sequelize.transaction(async t => {
+        let schemeNameExist = await models.scheme.findOne({ where: { schemeName } })
+
+    if (!check.isEmpty(schemeNameExist)) {
+        return res.status(404).json({ message: 'This Scheme Name is already Exist' });
+    }
         const addSchemeData = await models.scheme.create({
-            schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
+          schemeName,schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
             interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually
         });
 
@@ -32,30 +38,37 @@ exports.addScheme = async (req, res, next) => {
 
 //edit scheme
 
-exports.editScheme= async(req,res,next)=>{
-    let {partnerId,schemeId}=req.query;
-    const {schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
-        interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually}=req.body;
-        await sequelize.transaction(async t => {
+// exports.editScheme= async(req,res,next)=>{
+//     let {partnerId,schemeId}=req.query;
+//     const {schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
+//         interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually}=req.body;
+//         await sequelize.transaction(async t => {
 
-     let schemePartner= await models.partnerScheme.findOne({where:{partnerId,schemeId}});
-     if(schemePartner){
+//      let schemePartner= await models.partnerScheme.findOne({where:{partnerId,schemeId}});
+//      if(schemePartner){
          
-     let editScheme=await models.scheme.update({schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
-        interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually},{where:{id:schemeId}})
-return res.status(200).json({message:'updated'}),{transaction:t}
-}})
-return res.status(404).json({message:'updated failed'});
-}
+//      let editScheme=await models.scheme.update({schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
+//         interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually},{where:{id:schemeId}})
+// return res.status(200).json({message:'updated'}),{transaction:t}
+// }})
+// return res.status(404).json({message:'updated failed'});
+// }
 //read scheme
 
 exports.readScheme = async (req, res, next) => {
     let readSchemeData = await models.partner.findAll({
         where: { isActive: true },
-        include: [models.scheme],
+        include: [
+            {
+                model:models.scheme,
+            where:{
+            isActive:true
+        }
+    },
+],
     })
 
-    if (!readSchemeData) {
+    if (!readSchemeData[0]) {
         return res.status(404).json({ message: 'data not found' });
 
     }
@@ -93,25 +106,29 @@ exports.readSchemeByPartnerId = async (req, res, next) => {
 
 // update Scheme By id
 
-exports.updateScheme = async (req, res, next) => {
-    const schemeId = req.params.id;
-    const { schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
-        interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually } = req.body;
+// exports.updateScheme = async (req, res, next) => {
+//     const schemeId = req.params.id;
+//     const {schemeName,schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
+//         interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually } = req.body;
 
-    if (schemeAmountStart >= schemeAmountEnd) {
-        return res.status(400).json({ message: `Your Scheme start amount is must be greater than your Scheme end amount` })
-    }
+//     if (schemeAmountStart >= schemeAmountEnd) {
+//         return res.status(400).json({ message: `Your Scheme start amount is must be greater than your Scheme end amount` })
+//     }
+//     let schemeNameExist = await models.scheme.findOne({ where: { schemeName } });
 
-    const updateSchemeData = await models.scheme.update({
-        schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
-        interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually
-    }, { where: { id: schemeId, isActive: true } });
+//     if (!check.isEmpty(schemeNameExist)) {
+//         return res.status(404).json({ message: 'This Scheme Name is already Exist' });
+//     }
+//     const updateSchemeData = await models.scheme.update({
+//        schemeName, schemeAmountStart, schemeAmountEnd, interestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly,
+//         interestRateOneHundredEightyDaysMonthly, interestRateThirtyDaysAnnually, interestRateNinetyDaysAnnually, interestRateOneHundredEightyDaysAnnually
+//     }, { where: { id: schemeId, isActive: true } });
 
-    if (!updateSchemeData[0]) {
-        return res.status(404).json({ message: 'data not found' });
-    }
-    return res.status(200).json({ message: 'Success' });
-}
+//     if (!updateSchemeData[0]) {
+//         return res.status(404).json({ message: 'data not found' });
+//     }
+//     return res.status(200).json({ message: 'Success' });
+// }
 
 // delete Scheme by id
 
