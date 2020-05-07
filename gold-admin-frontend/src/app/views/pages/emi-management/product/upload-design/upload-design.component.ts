74b74808695928b774644b303586c693af7128ac
neebal <neebal@debian.debian>
2020-05-07 16:32:14 +0530

    Completed Upload Design Screen

--- gold-admin-frontend/src/app/views/pages/emi-management/product/upload-design/upload-design.component.ts
+++ gold-admin-frontend/src/app/views/pages/emi-management/product/upload-design/upload-design.component.ts
@@ -9,37 +9,32 @@ import { UploadDesignService } from '../../../../../core/emi-management/upload-d
   styleUrls: ['./upload-design.component.scss']
 })
 export class UploadDesignComponent implements OnInit {
-  public designs: FormGroup;
-  public file: any;
-  public arrayBuffer: any;
-  public exceljsondata: any;
-  public myFiles: Array<any> = [];
-  remark = '';
+  // public designs: FormGroup;
+  // public file: any;
+  // public arrayBuffer: any;
+  // public exceljsondata: any;
+  public files: Array<any> = [];
+  // remark = '';
   public frmData;
-  public spinnerValue: boolean = false;
-  excelFormat: Array<any> = [];
+  // excelFormat: Array<any> = [];
   public greaterThanFiftyImageError: boolean = false;
   public greaterThanFiftyImageButton: boolean = true;
 
   constructor(public fb: FormBuilder, public uploadDesignService: UploadDesignService, public toast: ToastrService, public ref: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.formdata();
+    // this.formdata();
   }
 
-  formdata() {
-    this.designs = this.fb.group({
-      design_input: ["", Validators.required]
-    });
-  }
+  // formdata() {
+  //   this.designs = this.fb.group({
+  //     design_input: ["", Validators.required]
+  //   });
+  // }
 
   getFileDetails(e) {
-    this.myFiles = [];
-    for (var i = 0; i < e.target.files.length; i++) {
-      this.myFiles.push(e.target.files[i]);
-
-    }
-    if (this.myFiles.length > 50) {
+    this.files = [];
+    if (e.target.files.length > 50) {
       this.greaterThanFiftyImageError = true;
       this.greaterThanFiftyImageButton = true;
     }
@@ -47,60 +42,69 @@ export class UploadDesignComponent implements OnInit {
       this.greaterThanFiftyImageError = false;
       this.greaterThanFiftyImageButton = false;
     }
+    if (e.target.files.length) {
+      for (const file of e.target.files) {
+        this.files.push(file);
+      }
+    }
   }
 
   uploadFiles() {
-    this.spinnerValue = true;
-    this.frmData = new FormData();
-    for (var i = 0; i < this.myFiles.length; i++) {
-      this.frmData.append("avatar", this.myFiles[i]);
-      if (i == 0) {
-        this.frmData.append("remark", this.remark);
-      }
+    if (!this.files.length) {
+      this.toast.error('Please first choose files to upload');
+      return;
     }
+
+    this.frmData = new FormData();
+    for (const file of this.files) {
+      this.frmData.append("avatar", file);
+    }
+    // for (var i = 0; i < this.files.length; i++) {
+    //   this.frmData.append("avatar", this.files[i]);
+    //   if (i == 0) {
+    //     this.frmData.append("remark", this.remark);
+    //   }
+    // }
     this.uploadDesignService.uploadMultipleImages(this.frmData).subscribe(
       res => {
-        this.excelFormat = []
         let errorLen = res.report.error.length;
         let succLen = res.report.success.length;
-        let length = Math.max(errorLen, succLen)
-        for (let i = 0; i < length; i++) {
-          let params = {
-            success: '',
-            error: ''
-          }
-          this.excelFormat.push(params)
-        }
-        for (let i = 0; i < length; i++) {
-          if (succLen > i)
-            this.excelFormat[i].success = res.report.success[i]
-          if (errorLen > i)
-            this.excelFormat[i].error = res.report.error[i]
-        }
-        // this.exportAsXLSX();
-        this.spinnerValue = false;
 
-        this.toast.success('Success', 'Designs Uploaded Successfully', {
-          timeOut: 3000
-        });
+        if (succLen >= errorLen) {
+          this.toast.success('Image Uploaded Successfully');
+        } else {
+          this.toast.error('All files not uploaded check image naming and upload again');
+        }
+
+        // let length = Math.max(errorLen, succLen)
+        // for (let i = 0; i < length; i++) {
+        //   if (succLen > i)
+        //     this.excelFormat[i].success = res.report.success[i]
+        //   if (errorLen > i)
+        //     this.excelFormat[i].error = res.report.error[i]
+        // }
+
+        this.files = [];
         this.ref.detectChanges();
       },
       err => {
-        this.spinnerValue = false;
+        this.files = [];
+        this.toast.error(err['error']['message']);
         this.ref.detectChanges();
-        this.toast.error('Sorry', err['error']['message'], {
-          timeOut: 3000
-        });
       }
     );
   }
 
-  isControlHasError(controlName: string, validationType: string): boolean {
-    const control = this.designs.controls[controlName];
-    if (!control) {
-      return false;
-    }
-    const result = control.hasError(validationType) && (control.dirty || control.touched);
-    return result;
+  cancelFiles() {
+    this.files = [];
   }
+
+  // isControlHasError(controlName: string, validationType: string): boolean {
+  //   const control = this.designs.controls[controlName];
+  //   if (!control) {
+  //     return false;
+  //   }
+  //   const result = control.hasError(validationType) && (control.dirty || control.touched);
+  //   return result;
+  // }
 }
