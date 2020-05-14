52030cefdc8d231376de79a2e819a9e90143e6d4
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-05-14 17:04:34 +0530

    get appraiser api ,customer uniquie id api created

--- gold-loan-backend/controllers/customer/customer.js
+++ gold-loan-backend/controllers/customer/customer.js
@@ -177,9 +177,9 @@ exports.editCustomer = async (req, res, next) => {
   let { id } = await models.status.findOne({ where: { statusName: "confirm" } })
 
   let customerExist = await models.customer.findOne({ where: { id: customerId } });
-if(id == customerExist.statusId){
-  return res.status(400).json({message: `This customer status is confirm, You cannot change any information of that customer.`})
-}
+  if (id == customerExist.statusId) {
+    return res.status(400).json({ message: `This customer status is confirm, You cannot change any information of that customer.` })
+  }
   if (check.isEmpty(customerExist)) {
     return res.status(404).json({ message: "Customer does not exist" });
   }
@@ -260,8 +260,8 @@ exports.getAllCustomers = async (req, res, next) => {
       as: "status",
     },
     {
-      model:models.internalBranch,
-      as:"internalBranch"
+      model: models.internalBranch,
+      as: "internalBranch"
     }
   ]
 
@@ -339,3 +339,29 @@ exports.filterCustomer = async (req, res) => {
   }
   return res.status(200).json({ customerFilterData });
 };
+
+exports.getCustomerUniqueId = async (req, res) => {
+  let customer = await models.customer.findAll({
+    attributes: ['id', 'customerUniqueId'],
+    include: [{
+      model: models.customerKyc,
+      as: 'customerKyc',
+      where: { kycStatus: "approved" },
+      attributes: []
+    }]
+  })
+  let assignCustomer = await models.customerAssignAppraiser.findAll({
+    attributes: ['id', 'customerUniqueId']
+  })
+  let arrayDiff = function (a, b) {
+    return a.filter(function (i) { return !b.find((c) => { return c.customerUniqueId == i.customerUniqueId }) });
+  };
+  let getDiff = (a, b) => {
+    let c = arrayDiff(a, b);
+    let d = arrayDiff(b, a);
+    return [...c, ...d]
+  }
+  
+  let diff = getDiff(customer, assignCustomer)
+  return res.status(200).json({ data: diff })
+}
