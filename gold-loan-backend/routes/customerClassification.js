8dd6ac18b32656086315bcabb7a4727a50fe6d76
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-05-18 10:05:18 +0530

    add checkRolePermission middleware

--- gold-loan-backend/routes/customerClassification.js
+++ gold-loan-backend/routes/customerClassification.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const route = express.Router();
 const { wrapper } = require('../utils/errorWrap');
-const { addCceRating, updateRating, readKycSubmmitedCustomer, addBranchManagerRating, readFirstStageVerifiedCustomer, readAllCustomerClassification, readAllCustomerClassificationById, updateCceRating } = require('../controllers/customerClassification/customerClassification')
+const { addCceRating, updateRating } = require('../controllers/customerClassification/customerClassification')
 
 const checkAuth = require('../middleware/checkAuth');
+const checkRolePermission = require('../middleware/checkRolesPermissions');
 
-route.post('/cce', checkAuth, wrapper(addCceRating));
 
-route.put('/', checkAuth, wrapper(updateRating))
+route.post('/cce', checkAuth, checkRolePermission, wrapper(addCceRating));
 
-route.put('/cce/:id', checkAuth, wrapper(updateCceRating));
+route.put('/', checkAuth, checkRolePermission, wrapper(updateRating))
 
-route.get('/cce', checkAuth, wrapper(readKycSubmmitedCustomer));
-
-route.post('/branch-manager', checkAuth, wrapper(addBranchManagerRating));
-
-route.get('/branch-manager', checkAuth, wrapper(readFirstStageVerifiedCustomer));
-
-route.get('/', checkAuth, wrapper(readAllCustomerClassification));
-
-route.get('/:id', checkAuth, wrapper(readAllCustomerClassificationById));
 
 module.exports = route;
