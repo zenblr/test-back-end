40d1105533587ad39a996be80813cb58f4320edd
neebal <neebal@debian.debian>
2020-05-20 16:14:12 +0530

    Changes in services

--- gold-admin-frontend/src/app/core/emi-management/product/category/services/category.service.ts
+++ gold-admin-frontend/src/app/core/emi-management/product/category/services/category.service.ts
@@ -14,9 +14,7 @@ export class CategoryService {
 	constructor(private http: HttpClient) { }
 
 	getAllCategories(from?, to?, search?): Observable<any> {
-		return this.http
-			.get<any>(`http://173.249.49.7:9120/api/category?search=${search}&from=${from}&to=${to}`, { observe: 'response' })
-			.pipe(map(response => response.body));
+		return this.http.get<any>(`http://173.249.49.7:9120/api/category?search=${search}&from=${from}&to=${to}`);
 	}
 
 	addCategory(data): Observable<any> {
