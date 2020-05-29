ef0287423454e7e3b1fe73831fd72a1944b80a7b
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-05-29 16:24:15 +0530

    partner dublication scheme functinaloty added

--- gold-loan-backend/controllers/scheme/uploadSchemes.js
+++ gold-loan-backend/controllers/scheme/uploadSchemes.js
@@ -16,17 +16,23 @@ exports.uploadScheme = async (req, res, next) => {
         if (jsonArray[i].AmountStart >= jsonArray[i].AmountEnd) {
             return res.status(400).json({ message: `Your Scheme start amount is must be greater than your Scheme end amount` })
         }
-        if(jsonArray[i].schemeName == '' || jsonArray[i].schemeName == undefined || jsonArray[i].schemeName == false){
+        if (jsonArray[i].schemeName == '' || jsonArray[i].schemeName == undefined || jsonArray[i].schemeName == false) {
             return res.status(400).json({ message: `Scheme name is required` })
         }
     }
-    let scheme = await jsonArray.map(value => { return value.schemeName })
+    let scheme = await jsonArray.map(value => { return value.schemeName.toLowerCase() })
     var repeatSchemeName = _.filter(scheme, (val, i, iteratee) => _.includes(iteratee, val, i + 1));
     if (repeatSchemeName.length > 0) {
         return res.status(400).json({ message: `In your csv file there scheme name is dublicate` })
     }
 
-    var contain = await models.scheme.findAll({ where: { schemeName: { [Op.in]: scheme } } })
+    var contain = await models.scheme.findAll({
+        where: { schemeName: { [Op.in]: scheme } },
+        include: [{
+            model: models.partner,
+            where: { id: partnerId[0] }
+        }]
+    })
     if (contain.length > 0) {
         let existSchemeName = await contain.map(value => { return value.schemeName })
 
@@ -39,7 +45,7 @@ exports.uploadScheme = async (req, res, next) => {
         for (var i = 0; i < jsonArray.length; i++) {
 
             let addSchemeData = await models.scheme.create({
-                schemeName: jsonArray[i].schemeName,schemeAmountStart: jsonArray[i].AmountStart, schemeAmountEnd: jsonArray[i].AmountEnd,
+                schemeName: jsonArray[i].schemeName.toLowerCase(),schemeAmountStart: jsonArray[i].AmountStart, schemeAmountEnd: jsonArray[i].AmountEnd,
                 interestRateThirtyDaysMonthly: jsonArray[i].InterestRateThirtyDaysMonthly, interestRateNinetyDaysMonthly: jsonArray[i].InterestRateNinetyDaysMonthly,
                 interestRateOneHundredEightyDaysMonthly: jsonArray[i].InterestRateOneHundredEightyDaysMonthly
             }, { transaction: t });
@@ -50,7 +56,7 @@ exports.uploadScheme = async (req, res, next) => {
 
                 }, { transaction: t })
             }
-            
+
     })
     return res.status(201).json({ message: " Schemes Created" })
 
