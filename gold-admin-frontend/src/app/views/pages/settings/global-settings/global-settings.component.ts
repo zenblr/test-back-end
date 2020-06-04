a0bc8474acb60bd467016c298e5d336fa5a517db
Bhushan Madaye <bhushanmadaye@nimapinfotech.com>
2020-06-04 11:45:53 +0530

    bhushAN: global settings

--- gold-admin-frontend/src/app/views/pages/settings/global-settings/global-settings.component.ts
+++ gold-admin-frontend/src/app/views/pages/settings/global-settings/global-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { GlobalSettingService } from '../../../../core/global-setting/services/global-setting.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'kt-global-settings',
@@ -14,7 +16,8 @@ export class GlobalSettingsComponent implements OnInit {
   gracePeriodForm: FormGroup;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private globalSettingService: GlobalSettingService
   ) { }
 
   ngOnInit() {
@@ -43,8 +46,37 @@ export class GlobalSettingsComponent implements OnInit {
     })
   }
 
-  submit() {
-    
+  submit(type) {
+    let formData = {};
+    switch (type) {
+      case 'ltvGoldPercent':
+        formData = { data: this.ltvGoldPercentForm.value, method: type }
+        break;
+
+      case 'minimumLoanAmount':
+        formData = { data: this.minimumLoanAmountForm.value, method: type }
+        break;
+
+      case 'minimumTopUpAmount':
+        formData = { data: this.minimumTopUpAmountForm.value, method: type }
+        break;
+
+      case 'gracePeriod':
+        formData = { data: this.gracePeriodForm.value, method: type }
+        break;
+
+      case 'gst':
+        formData = { data: this.gstForm.value, method: type }
+        break;
+
+      default:
+        break;
+    }
+
+    this.globalSettingService.setGlobalSetting(formData).pipe(map(res => {
+      console.log(res);
+    })).subscribe()
+
   }
 
 }
