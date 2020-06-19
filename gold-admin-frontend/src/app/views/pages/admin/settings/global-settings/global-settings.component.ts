00f2883a28d2b9239d8a84862d4645a03cf25984
Bhushan Madaye <bhushanmadaye@nimapinfotech.com>
2020-06-19 18:56:24 +0530

    bhushan::global-settings

--- gold-admin-frontend/src/app/views/pages/admin/settings/global-settings/global-settings.component.ts
+++ gold-admin-frontend/src/app/views/pages/admin/settings/global-settings/global-settings.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GlobalSettingService } from '../../../../../core/global-setting/services/global-setting.service';
 import { map } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'kt-global-settings',
@@ -15,77 +16,113 @@ export class GlobalSettingsComponent implements OnInit {
   minimumTopUpAmountForm: FormGroup;
   gracePeriodForm: FormGroup;
   cashTransactionLimitForm: FormGroup;
+  globalSettingForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private globalSettingService: GlobalSettingService
+    private globalSettingService: GlobalSettingService,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit() {
     this.initForm();
+    this.getGlobalSetting()
   }
 
   initForm() {
-    this.ltvGoldPercentForm = this.fb.group({
-      ltvGoldPercent: []
+    this.globalSettingForm = this.fb.group({
+      ltvGoldValue: [, [Validators.required]],
+      minimumLoanAmountAllowed: [, [Validators.required]],
+      gst: [, [Validators.required]],
+      cashTransactionLimit: [, [Validators.required]],
+      minimumTopUpAmount: [, [Validators.required]],
+      gracePeriodDays: [, [Validators.required]]
     })
 
-    this.minimumLoanAmountForm = this.fb.group({
-      minimumLoanAmount: []
-    })
+    // this.minimumLoanAmountForm = this.fb.group({
+    //   minimumLoanAmount: []
+    // })
 
-    this.gstForm = this.fb.group({
-      gst: []
-    })
+    // this.gstForm = this.fb.group({
+    //   gst: []
+    // })
 
-    this.cashTransactionLimitForm = this.fb.group({
-      cashTransactionLimit: []
-    })
+    // this.cashTransactionLimitForm = this.fb.group({
+    //   cashTransactionLimit: []
+    // })
 
-    this.minimumTopUpAmountForm = this.fb.group({
-      minimumTopUpAmount: []
-    })
+    // this.minimumTopUpAmountForm = this.fb.group({
+    //   minimumTopUpAmount: []
+    // })
 
-    this.gracePeriodForm = this.fb.group({
-      gracePeriod: []
-    })
+    // this.gracePeriodForm = this.fb.group({
+    //   gracePeriod: []
+    // })
   }
 
-  submit(type) {
-    let formData = {};
-    switch (type) {
-      case 'ltvGoldPercent':
-        formData = { data: this.ltvGoldPercentForm.value, method: type }
-        break;
+  getGlobalSetting() {
 
-      case 'minimumLoanAmount':
-        formData = { data: this.minimumLoanAmountForm.value, method: type }
-        break;
-
-      case 'minimumTopUpAmount':
-        formData = { data: this.minimumTopUpAmountForm.value, method: type }
-        break;
-
-      case 'gracePeriod':
-        formData = { data: this.gracePeriodForm.value, method: type }
-        break;
-
-      case 'cashTransactionLimit':
-        formData = { data: this.cashTransactionLimitForm.value, method: type }
-        break;
-
-      case 'gst':
-        formData = { data: this.gstForm.value, method: type }
-        break;
-
-      default:
-        break;
+    if (this.globalSettingService.globalSetting.getValue() != null) {
+      this.globalSettingService.globalSetting$.subscribe(res => {
+        this.globalSettingForm.patchValue(res);
+      })
+    } else {
+      this.globalSettingService.getGlobalSetting().pipe(map(res => {
+        this.globalSettingService.globalSetting.next(res);
+        this.globalSettingForm.patchValue(res);
+      })).subscribe()
     }
 
+  }
+
+  submit() {
+    // let formData = {};
+    // switch (type) {
+    //   case 'ltvGoldPercent':
+    //     formData = { data: this.ltvGoldPercentForm.value, method: type }
+    //     break;
+
+    //   case 'minimumLoanAmount':
+    //     formData = { data: this.minimumLoanAmountForm.value, method: type }
+    //     break;
+
+    //   case 'minimumTopUpAmount':
+    //     formData = { data: this.minimumTopUpAmountForm.value, method: type }
+    //     break;
+
+    //   case 'gracePeriod':
+    //     formData = { data: this.gracePeriodForm.value, method: type }
+    //     break;
+
+    //   case 'cashTransactionLimit':
+    //     formData = { data: this.cashTransactionLimitForm.value, method: type }
+    //     break;
+
+    //   case 'gst':
+    //     formData = { data: this.gstForm.value, method: type }
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+    if (this.globalSettingForm.invalid) {
+      this.globalSettingForm.markAllAsTouched();
+      return
+    }
+
+    const formData = this.globalSettingForm.value;
+
     this.globalSettingService.setGlobalSetting(formData).pipe(map(res => {
-      console.log(res);
+      if (res) {
+        console.log(res);
+        this.toastr.success('Successful!')
+      }
     })).subscribe()
 
+  }
+
+  get controls() {
+    return this.globalSettingForm.controls;
   }
 
 }
