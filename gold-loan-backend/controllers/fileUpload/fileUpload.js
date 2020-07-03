71f6b6667720287fddb599a8e93d582b4c5ff356
pranali95 <pranali@nimapinfotech.com>
2020-07-03 16:38:13 +0530

    aws file upload base64

--- gold-loan-backend/controllers/fileUpload/fileUpload.js
+++ gold-loan-backend/controllers/fileUpload/fileUpload.js
@@ -1,41 +1,41 @@
 const multer = require('multer');
 
 const models = require('../../models'); // importing models.
-
+const multerS3 = require('multer-s3');
 const { BASEURL } = require('../../config/baseUrl');
 const fs = require('fs');
+let AWS = require('aws-sdk');
 
 // File Upload.
 exports.uploadFile =
     async (req, res, next) => {
-    const fileFor = req.query.reason;
-    const loanId = req.query.loanId;
-    const customerId = req.query.customerId;
-    const partnerId = req.query.partnerId
-    
-    let destination;
-    if(fileFor == "user"){
-        destination = `public/uploads/user/`
-    }else if(fileFor == "loan"){
-       destination = `public/uploads/loan/${loanId}/`;
-    }else if(fileFor == "customer"){
-        destination = `public/uploads/customer/${customerId}/`;
-    }else if(fileFor == "lead"){
-        destination = `public/uploads/lead/`;
-    }else if(fileFor == "banner"){
-        destination = 'public/uploads/banner/';
-    }else if(fileFor == "offer"){
-        destination = 'public/uploads/offer/';
-    }else if(fileFor == "lenderBanner"){
-        destination = 'public/uploads/lenderBanner/';
-    }else if(fileFor == "scheme"){
-        destination = `public/uploads/scheme/${partnerId}/`;
-    }else if(fileFor == "holiday"){
-        destination = `public/uploads/holiday/`
-    }else{
-        return res.status(422).json({ message: 'reason not found' });
-    }
+        const fileFor = req.query.reason;
+        const loanId = req.query.loanId;
+        const customerId = req.query.customerId;
+        const partnerId = req.query.partnerId
 
+        let destination;
+        if (fileFor == "user") {
+            destination = `public/uploads/user/`
+        } else if (fileFor == "loan") {
+            destination = `public/uploads/loan/${loanId}/`;
+        } else if (fileFor == "customer") {
+            destination = `public/uploads/customer/${customerId}/`;
+        } else if (fileFor == "lead") {
+            destination = `public/uploads/lead/`;
+        } else if (fileFor == "banner") {
+            destination = 'public/uploads/banner/';
+        } else if (fileFor == "offer") {
+            destination = 'public/uploads/offer/';
+        } else if (fileFor == "lenderBanner") {
+            destination = 'public/uploads/lenderBanner/';
+        } else if (fileFor == "scheme") {
+            destination = `public/uploads/scheme/${partnerId}/`;
+        } else if (fileFor == "holiday") {
+            destination = `public/uploads/holiday/`
+        } else {
+            return res.status(422).json({ message: 'reason not found' });
+        }
         const storage = multer.diskStorage({
             filename: (req, file, cb) => {
                 const extArray = file.originalname.split('.');
@@ -44,9 +44,7 @@ exports.uploadFile =
             },
             destination: destination,
         });
-
         const uploads = multer({ storage }).single('avatar');
-
         uploads(req, res, async (err) => {
             if (err) {
                 res.status(500);
@@ -64,7 +62,6 @@ exports.uploadFile =
                     uploadFile
                 });
             }
-
         });
     };
 
@@ -79,26 +76,47 @@ exports.getFile = async (req, res, next) => {
 }
 
 exports.base64Convertor = async (req, res, next) => {
-
-    let destination = 'public/uploads/images/',
-
-        baseImage = req.body.avatar.split(',')
-    let mimetype = baseImage[0].split(';')[0].split(':')[1]
     let fileName = Date.now();
-    let encoding = '7Bit';
     let userId = req.userData.id
-    let url = destination + `${fileName}.jpeg`;
+    let destination = 'public/uploads/images/';
+    if (process.env.FILE_TO_AWS == 'true') {
+        AWS.config.update({
+            secretAccessKey: `${process.env.Secretkey}`,
+            accessKeyId: `${process.env.Accessid}`,
+            region: process.env.Region,
+        });
+        let s3 = new AWS.S3({ params: { Bucket: process.env.Bucket } });
+        let buf = Buffer.from(req.body.avatar.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+        var data = {
+            Key: `${destination}${fileName}.jpeg`,
+            Body: buf,
+            ACL: 'public-read',
+            ContentEncoding: 'base64',
+            ContentType: 'image/jpeg'
+        };
+        const awsData = await s3.upload(data).promise();
+        console.log(awsData)
+        let uploadFile = await models.fileUpload.create({ filename: `${fileName}.jpeg`, url: awsData.Location, path: awsData.Key, userId: userId });
+        return res.status(200).json({
+            imgUrl: awsData.Location, uploadFile
+        })
+    } else {
+        let baseImage = req.body.avatar.split(',')
+        let mimetype = baseImage[0].split(';')[0].split(':')[1]
+        let encoding = '7Bit';
+        let userId = req.userData.id
+        let url = destination + `${fileName}.jpeg`;
 
-    let pathToadd = destination.replace('public/', '');
-    let path = pathToadd  + `${fileName}.jpeg`;
+        let pathToadd = destination.replace('public/', '');
+        let path = pathToadd + `${fileName}.jpeg`;
 
-    let bitmap = await new Buffer.from(baseImage[1], 'base64')
-    fs.writeFileSync(`public/uploads/images/${fileName}.jpeg`, bitmap)
+        let bitmap = await new Buffer.from(baseImage[1], 'base64')
+        fs.writeFileSync(`public/uploads/images/${fileName}.jpeg`, bitmap)
 
-    let uploadFile = await models.fileUpload.create({ filename: `${fileName}.jpeg`, url: url, mimetype: mimetype, encoding: encoding,path: path, userId: userId });
+        let uploadFile = await models.fileUpload.create({ filename: `${fileName}.jpeg`, url: url, mimetype: mimetype, encoding: encoding, path: path, userId: userId });
 
-    return res.status(200).json({
-        imgUrl: `${BASEURL}/uploads/images/${fileName}.jpeg`, uploadFile
-    })
-
+        return res.status(200).json({
+            imgUrl: `${BASEURL}/uploads/images/${fileName}.jpeg`, uploadFile
+        })
+    }
 }
