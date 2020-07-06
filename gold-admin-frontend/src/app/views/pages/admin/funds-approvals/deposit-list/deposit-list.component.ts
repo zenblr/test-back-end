97a38150a970e6bee47945a5a49691c82f0ea059
Bhushan Madaye <bhushanmadaye@nimapinfotech.com>
2020-07-06 17:35:25 +0530

    bhushan::deposit UI

--- gold-admin-frontend/src/app/views/pages/admin/funds-approvals/deposit-list/deposit-list.component.ts
+++ gold-admin-frontend/src/app/views/pages/admin/funds-approvals/deposit-list/deposit-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataTableService } from '../../../../../core/shared/services/data-table.service';
+import { MatPaginator, MatDialog } from '@angular/material';
+import { Subject, Subscription, merge } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { LayoutUtilsService } from '../../../../../core/_base/crud';
+import { map, tap } from 'rxjs/operators';
+import { takeUntil, skip, distinctUntilChanged } from 'rxjs/operators';
+import { DepositDatasource } from '../../../../../core/funds-approvals/deposit/datasources/deposit.datasource';
+import { DepositService } from '../../../../../core/funds-approvals/deposit/services/deposit.service';
 
 @Component({
   selector: 'kt-deposit-list',
@@ -7,9 +16,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositListComponent implements OnInit {
 
-  constructor() { }
+  dataSource;
+  displayedColumns = ['transactionId', 'bankTransactionId', 'customerId', 'loanId', 'depositDate', 'fullName', 'mobileNumber', 'modeOfPayment', 'depositBankName', 'depositAmount', 'depositStatus'];
+  result = []
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  unsubscribeSearch$ = new Subject();
+  destroy$ = new Subject();
+  searchValue = '';
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private dataTableService: DataTableService,
+    private depositService: DepositService,
+    public dialog: MatDialog,
+    private toastr: ToastrService,
+    private layoutUtilsService: LayoutUtilsService,
+  ) { }
 
   ngOnInit() {
+    const paginatorSubscriptions = merge(this.paginator.page).pipe(
+      tap(() => this.loadPage())
+    ).subscribe();
+    this.subscriptions.push(paginatorSubscriptions);
+
+
+    const searchSubscription = this.dataTableService.searchInput$.pipe(takeUntil(this.unsubscribeSearch$))
+      .subscribe(res => {
+        this.searchValue = res;
+        this.paginator.pageIndex = 0;
+        this.loadPage();
+      });
+
+    this.dataSource = new DepositDatasource(this.depositService);
+    const entitiesSubscription = this.dataSource.entitySubject.pipe(
+      skip(1),
+      distinctUntilChanged()
+    ).subscribe(res => {
+      this.result = res;
+    });
+    this.subscriptions.push(entitiesSubscription);
+
+    this.dataSource.getDepositList(1, 25, this.searchValue);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(el => el.unsubscribe());
+    this.unsubscribeSearch$.next();
+    this.unsubscribeSearch$.complete();
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+  loadPage() {
+    if (this.paginator.pageIndex < 0 || this.paginator.pageIndex > (this.paginator.length / this.paginator.pageSize))
+      return;
+    let from = ((this.paginator.pageIndex * this.paginator.pageSize) + 1);
+    let to = ((this.paginator.pageIndex + 1) * this.paginator.pageSize);
+
+    this.dataSource.getDepositList(from, to, this.searchValue);
   }
 
 }
