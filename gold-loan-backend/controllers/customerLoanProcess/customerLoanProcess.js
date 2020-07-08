27feb29422239d57535bafdefe6a56f716eb7cd8
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-07-08 14:18:58 +0530

    disbursement complete date change of emi

--- gold-loan-backend/controllers/customerLoanProcess/customerLoanProcess.js
+++ gold-loan-backend/controllers/customerLoanProcess/customerLoanProcess.js
@@ -6,6 +6,7 @@ const Op = Sequelize.Op;
 const paginationFUNC = require('../../utils/pagination'); // IMPORTING PAGINATION FUNCTION
 
 const check = require("../../lib/checkLib"); // IMPORTING CHECKLIB 
+const moment = require('moment')
 
 
 //  FUNCTION FOR GET CUSTOMER DETAILS AFTER ENTER UNIQUE ID DONE
@@ -861,24 +862,64 @@ exports.disbursementOfLoanAmount = async (req, res, next) => {
     let Loan = await models.customerLoanMaster.findOne({
         where: { id: masterLoanId },
         attributes: ['paymentFrequency'],
+        order: [
+            [models.customerLoanInterest, "id", "asc"],
+        ],
         include: [{
             model: models.customerLoanInterest,
             as: 'customerLoanInterest',
             where: { isActive: true, loanId: loanId }
         }]
     })
+    let startDate = Loan.customerLoanInterest[0].emiDueDate;
+    let endDate = Loan.customerLoanInterest[Loan.customerLoanInterest.length - 1].emiDueDate;
+
+    let holidayDate = await models.holidayMaster.findAll({
+        attributes: ['holidayDate'],
+        where: {
+            holidayDate: {
+                [Op.between]: [startDate, endDate]
+            }
+        }
+    })
+
     let table = Loan.customerLoanInterest;
 
     for (let i = 0; i < table.length; i++) {
         let date = new Date();
-        let newEmiDueDate = new Date(date.setDate(date.getDate() + (Number(masterLoan.paymentFrequency) * (i + 1))))
+        let newEmiDueDate = new Date(date.setDate(date.getDate() + (Number(Loan.paymentFrequency) * (i + 1))))
         table[i].emiDueDate = newEmiDueDate
+        for (let j = 0; j < holidayDate.length; j++) {
+            let momentDate = moment(newEmiDueDate, "DD-MM-YYYY").format('YYYY-MM-DD')
+            if (momentDate == holidayDate[j].holidayDate) {
+                let newDate = new Date(newEmiDueDate);
+                let holidayEmiDueDate = new Date(newDate.setDate(newDate.getDate() + 1))
+                table[i].emiDueDate = holidayEmiDueDate
+                newEmiDueDate = holidayEmiDueDate
+                j = 0
+            }
+        }
+        table.loanId = loanId
+        table.masterLoanId = masterLoanId
     }
+
+    let newStartDate = date
+    let newEndDate = table[table.length - 1].emiDueDate
+
+
 
     if (loanDetails.loanStageId == matchStageId.id) {
 
         await sequelize.transaction(async (t) => {
+
+            await models.customerLoanMaster.update({ loanStartDate: newStartDate, loanEndDate: newEndDate }, { where: { id: masterLoanId }, transaction: t })
+
+            for (let a = 0; a < table.length; a++) {
+                let updateDate = table[a].emiDueDate
+                await models.customerLoanInterest.update({ emiDueDate: updateDate }, { where: { id: table[a].id }, transaction: t })
+            }
             await models.customerLoan.update({ disbursed: true }, { where: { id: loanId }, transaction: t })
+
             await models.customerLoanDisbursement.create({
                 loanId, masterLoanId, loanAmount, transactionId, date, paymentMode, ifscCode, bankName, bankBranch,
                 accountHolderName, accountNumber, disbursementStatus, createdBy, modifiedBy
@@ -897,6 +938,7 @@ exports.disbursementOfLoanAmount = async (req, res, next) => {
             for (let ele of masterLoan.customerLoan) {
                 approved.push(ele.disbursed)
             }
+
             if (!approved.includes(false)) {
                 await models.customerLoanMaster.update({ loanStageId: stageId.id }, { where: { id: masterLoanId }, transaction: t })
             }
