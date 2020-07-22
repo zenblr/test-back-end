ac13206390fbfbdfda65509bff05e81778c2f73a
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-07-22 19:20:20 +0530

    quick pay api added

--- gold-loan-backend/routes/quickPay.js
+++ gold-loan-backend/routes/quickPay.js
@@ -1,11 +1,13 @@
 const { wrapper } = require('../utils/errorWrap');
 const checkAuth = require('../middleware/checkAuth');
-const { getInterestTable } = require('../controllers/quickPay/quickPay');
+const { getInterestTable, getInterestInfo } = require('../controllers/quickPay/quickPay');
 
 const express = require('express');
-const route=express.Router();
+const route = express.Router();
 
 route.get('/interest-table', checkAuth, wrapper(getInterestTable));
 
+route.get('/interest-info', checkAuth, wrapper(getInterestInfo))
 
-module.exports=route;   
+
+module.exports = route;   
