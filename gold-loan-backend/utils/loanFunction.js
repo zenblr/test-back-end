ca82642d3f22eceb6cf0861df659dcf58542ff8f
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-07-24 17:47:19 +0530

    changes in quick pay

--- gold-loan-backend/utils/loanFunction.js
+++ gold-loan-backend/utils/loanFunction.js
@@ -6,7 +6,7 @@ exports.mergeInterestTable = async (masterLoanId) => {
 
     let interestTable = await models.customerLoanMaster.findOne({
         where: { id: masterLoanId },
-        attributes: ['id'],
+        attributes: ['id','isUnsecuredSchemeApplied'],
         order: [
             [models.customerLoan, 'id', 'asc'],
             [models.customerLoan, models.customerLoanInterest, 'id', 'asc'],
@@ -22,9 +22,11 @@ exports.mergeInterestTable = async (masterLoanId) => {
             }]
         }]
     })
-
     let securedTable = interestTable.customerLoan[0].customerLoanInterest
-    let unsecuredTable = interestTable.customerLoan[1].customerLoanInterest
+    let unsecuredTable
+    if(interestTable.isUnsecuredSchemeApplied){
+        unsecuredTable = interestTable.customerLoan[1].customerLoanInterest
+    }
 
     let mergeTble = []
 
@@ -32,12 +34,20 @@ exports.mergeInterestTable = async (masterLoanId) => {
         let data = {}
         data.masterLoanId = securedTable[i].masterLoanId
         data.emiDueDate = securedTable[i].emiDueDate
-        data.interestAmount = (securedTable[i].interestAmount + unsecuredTable[i].interestAmount).toFixed(2)
-        data.balanceAmount = (securedTable[i].balanceAmount + unsecuredTable[i].balanceAmount).toFixed(2)
-        data.paidAmount = securedTable[i].paidAmount + unsecuredTable[i].paidAmount
-        data.panelInterest = securedTable[i].panelInterest + unsecuredTable[i].panelInterest
         data.emiStatus = securedTable[i].emiStatus
+        if(interestTable.isUnsecuredSchemeApplied){
+            data.emiReceivedDate = securedTable[i].emiReceivedDate
+            data.interestAmount = (securedTable[i].interestAmount + unsecuredTable[i].interestAmount).toFixed(2)
+            data.balanceAmount = (securedTable[i].balanceAmount + unsecuredTable[i].balanceAmount).toFixed(2)
+            data.paidAmount = securedTable[i].paidAmount + unsecuredTable[i].paidAmount
+            data.panelInterest = securedTable[i].panelInterest + unsecuredTable[i].panelInterest
+        }
         data.emiReceivedDate = securedTable[i].emiReceivedDate
+        data.interestAmount = (securedTable[i].interestAmount).toFixed(2)
+        data.balanceAmount = (securedTable[i].balanceAmount).toFixed(2)
+        data.paidAmount = securedTable[i].paidAmount
+        data.panelInterest = securedTable[i].panelInterest
+    
         mergeTble.push(data)
     };
 
