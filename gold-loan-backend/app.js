1e27078c37984630a6a7ec45f08ade117a208255
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-07-29 13:00:47 +0530

    changes limit

--- gold-loan-backend/app.js
+++ gold-loan-backend/app.js
@@ -35,12 +35,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({
     extended: true,
-    limit: '50mb',
+    limit: '500mb',
     parameterLimit: 1000000
 }));
 // app.use(express.urlencoded({ extended: false }));
