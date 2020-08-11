d8fbe7bea3e2933761c959f3295a921fa5ab2638
Ravi kumar <ravikumar@nimapinfotech.com>
2020-08-11 17:35:13 +0530

    calculation changes

--- gold-loan-backend/controllers/customerLoanProcess/customerLoanProcess.js
+++ gold-loan-backend/controllers/customerLoanProcess/customerLoanProcess.js
@@ -460,12 +460,14 @@ exports.generateInterestTable = async (req, res, next) => {
 
     if (!Number.isInteger(length)) {
         const lastElementOfTable = interestTable[interestTable.length - 1]
-        let secure = (securedInterestAmount / Math.ceil(length)).toFixed(2)
+        const oneMonthSecured = securedInterestAmount / (paymentFrequency/30)
+        let secure = (oneMonthSecured * Math.ceil(length)).toFixed(2)
         lastElementOfTable.securedInterestAmount = secure
 
         if (isUnsecuredSchemeApplied) {
-            let unsecure = (unsecuredInterestAmount / Math.ceil(length)).toFixed(2)
-            lastElementOfTable.unsecuredInterestAmount = unsecure
+            const  oneMonthUnsecured = unsecuredInterestAmount / (paymentFrequency/30)
+            let unsecured = (oneMonthUnsecured * Math.ceil(length)).toFixed(2)
+            lastElementOfTable.unsecuredInterestAmount = unsecured
             lastElementOfTable.totalAmount = Number(lastElementOfTable.securedInterestAmount) + Number(lastElementOfTable.unsecuredInterestAmount)
         }
 
