8931306a8bc30435cbf4c552b2124fa62f9bcb0c
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-08-11 19:20:17 +0530

    quick pay api changes

--- gold-loan-backend/controllers/quickPay/quickPay.js
+++ gold-loan-backend/controllers/quickPay/quickPay.js
@@ -12,7 +12,7 @@ const CONSTANT = require("../../utils/constant");
 const check = require("../../lib/checkLib");
 const { paginationWithFromTo } = require("../../utils/pagination");
 let sms = require('../../utils/sendSMS');
-let { mergeInterestTable, getCustomerInterestAmount } = require('../../utils/loanFunction')
+let { mergeInterestTable, getCustomerInterestAmount, getLoanDetails } = require('../../utils/loanFunction')
 
 //INTEREST TABLE 
 exports.getInterestTable = async (req, res, next) => {
@@ -54,20 +54,49 @@ exports.getInterestInfo = async (req, res, next) => {
 
 //CALCULATE PAYABLE AMOUNT
 exports.payableAmount = async (req, res, next) => {
-    let { id } = req.query;
-    let amount = await getCustomerInterestAmount(id);
+    let { masterLoanId } = req.query;
+    let amount = await getCustomerInterestAmount(masterLoanId);
 
+    let loan = await getLoanDetails(masterLoanId);
+
+    let interest = amount.secured.interest
+    let penalInterest = amount.secured.penalInterest
     let payableAmount = amount.secured.interest + amount.secured.penalInterest
     if (amount.unsecured) {
         payableAmount = payableAmount + amount.unsecured.interest + amount.unsecured.penalInterest
-    }
+        interest = interest + amount.unsecured.interest
+        penalInterest = penalInterest + amount.unsecured.penalInterest
 
-    return res.status(200).json({ data: (payableAmount.toFixed(2)), amount });
+    }
+    let data = {}
+    data.outstandingAmount = (loan.outstandingAmount).toFixed(2)
+    data.interest = (interest).toFixed(2)
+    data.penalInterest = (penalInterest).toFixed(2)
+    data.payableAmount = (payableAmount).toFixed(2)
+
+    return res.status(200).json({ data });
 }
 
 //CALCULATE PAYABLE AMOUNT
-exports.payableAmountForCustomer = async (req, res, next) => {
+exports.payableAmountConfirm = async (req, res, next) => {
+    let { masterLoanId, amount } = req.query
 
-    let { masterLoanId } = req.query
+    let loan = await models.customerLoanMaster.findOne({
+        where: { isActive: true, id: masterLoanId },
+        attributes: ['id', 'outstandingAmount', 'finalLoanAmount', 'tenure'],
+        order: [
+            [models.customerLoan, 'id', 'desc']
+        ],
+        include: [{
+            model: models.customerLoan,
+            as: 'customerLoan',
+            attributes: ['id', 'loanUniqueId', 'outstandingAmount', 'loanAmount'],
+            where: { isActive: true }
+        }]
+    });
+    console.log(amount)
+    loan['amount'] = amount
+    return res.status(200).json({ data: loan });
+
 }
 
