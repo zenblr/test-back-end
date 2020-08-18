8b753fb5f90d2e03b482e1c423b83b5008e57aa4
pranali95 <pranali@nimapinfotech.com>
2020-08-18 20:47:09 +0530

    fuull release api

---
+++ gold-loan-backend/utils/fullReleaseHistory.js
@@ -0,0 +1,12 @@
+module.exports = {
+    FULL_RELEASE_PAYMENT_CASH: "Full release payment by cash",
+    FULL_RELEASE_PAYMENT_CHEQUE: "Full release payment by cheque",
+    FULL_RELEASE_PAYMENT_BANK: "Full release payment by IMPS/NEFT/RTGS",
+    FULL_RELEASE_AMOUNT_STATUS_C:"Amount status changed to completed",
+    FULL_RELEASE_AMOUNT_STATUS_R:"Amount status changed to rejected",
+    FULL_RELEASE_ASSIGNED_APPRAISER:"Assigned appraiser for process",
+    FULL_RELEASE_UPDATED_APPRAISER:"Updated appraiser for process",
+    FULL_RELEASE_STATUS_P:"Amount status changed to pending",
+    FULL_RELEASE_STATUS_R:"Amount status changed to released",
+    FULL_RELEASE_DOCUMENT:"Full release documents uploaded",
+}
