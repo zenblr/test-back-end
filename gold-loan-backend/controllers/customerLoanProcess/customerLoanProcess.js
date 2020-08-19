61df780d388576a555c9e7aa800e9e2d0f7bf9b9
Ravi kumar <ravikumar@nimapinfotech.com>
2020-08-19 23:19:02 +0530

    changes in calculation

--- gold-loan-backend/controllers/customerLoanProcess/customerLoanProcess.js
+++ gold-loan-backend/controllers/customerLoanProcess/customerLoanProcess.js
@@ -341,17 +341,17 @@ exports.checkForLoanType = async (req, res, next) => {
                 // Math.round(loanAmount) >= Math.round(securedLoanAmount + unsecuredAmount)) ||
                 // Math.round(loanAmount) >= Math.round(securedLoanAmount + unsecuredAmount)) ||
                 // isLoanTransfer) {
-                    if (Math.round(loanAmount) > Math.round(securedLoanAmount + unsecuredAmount)) {
-                        return res.status(400).json({ message: "No Unsecured Scheme Availabe" })
-    
-                    }
+                if (Math.round(loanAmount) > Math.round(securedLoanAmount + unsecuredAmount)) {
+                    return res.status(400).json({ message: "No Unsecured Scheme Availabe" })
+
+                }
                 // if (isLoanTransfer) {
                 //     if (Number(loanAmount) > totalEligibleAmt) {
-                    if(!securedScheme.isSplitAtBeginning){
-                        securedLoanAmount = Math.round(fullAmount * secureSchemeMaximumAmtAllowed)
-                        unsecuredAmount = Number(loanAmount - securedLoanAmount)
-                    }
-                    // }
+                if (!securedScheme.isSplitAtBeginning) {
+                    securedLoanAmount = Math.round(fullAmount * secureSchemeMaximumAmtAllowed)
+                    unsecuredAmount = Number(loanAmount - securedLoanAmount)
+                }
+                // }
                 // } else if (Math.round(loanAmount) > Math.round(securedLoanAmount + unsecuredAmount)) {
                 //     return res.status(400).json({ message: "No Unsecured Scheme Availabe" })
 
@@ -369,11 +369,29 @@ exports.checkForLoanType = async (req, res, next) => {
 
                 var unsecuredAmount = Math.round(fullAmount * newUnsecuredMaximum)
 
+
                 if (isLoanTransfer) {
                     if (Number(loanAmount) > totalEligibleAmt) {
                         securedLoanAmount = Math.round(fullAmount * secureSchemeMaximumAmtAllowed)
                         unsecuredAmount = Number(loanAmount - securedLoanAmount)
+
+
+                    } else {
+                       
+                        unsecuredAmount = Number(loanAmount - securedLoanAmount)
                     }
+
+                    processingCharge = await processingChargeSecuredScheme(securedLoanAmount, securedScheme, unsecuredSchemeApplied, unsecuredAmount)
+
+                    let newUnsecuredScheme = await selectScheme(unsecured, securedScheme)
+                    return res.status(200).json({ data: { newUnsecuredScheme, unsecuredScheme, unsecuredAmount, securedLoanAmount, processingCharge, unsecuredSchemeApplied, securedScheme, isUnsecuredSchemeApplied: true } })
+                }
+
+                if (!securedScheme.isSplitAtBeginning) {
+                    securedLoanAmount = Math.round(fullAmount * secureSchemeMaximumAmtAllowed)
+                    unsecuredAmount = Number(loanAmount - securedLoanAmount)
+                } else {
+                    unsecuredAmount = Number(loanAmount - securedLoanAmount)
                 }
 
                 processingCharge = await processingChargeSecuredScheme(securedLoanAmount, securedScheme, unsecuredSchemeApplied, unsecuredAmount)
