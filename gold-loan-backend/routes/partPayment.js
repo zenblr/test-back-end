0be5bce1f3b715138bde9c162f32e0059291116d
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-08-21 11:28:12 +0530

    changes in part payment

--- gold-loan-backend/routes/partPayment.js
+++ gold-loan-backend/routes/partPayment.js
@@ -1,6 +1,6 @@
 const { wrapper } = require('../utils/errorWrap');
 const checkAuth = require('../middleware/checkAuth');
-const { getInterestInfo, checkPartAmount, partPayment } = require('../controllers/partPayment/partPayment');
+const { getInterestInfo, checkPartAmount, partPayment, payableAmountConfirmPartPayment } = require('../controllers/partPayment/partPayment');
 
 const express = require('express');
 const route = express.Router();
@@ -8,6 +8,8 @@ const route = express.Router();
 route.get('/interest-info', checkAuth, wrapper(getInterestInfo))
 
 route.post('/check-part-amount', checkAuth, wrapper(checkPartAmount))
+
+route.get('/confirm-payment-info', checkAuth, wrapper(payableAmountConfirmPartPayment))
 
 route.post('/payment', checkAuth, wrapper(partPayment))
 
