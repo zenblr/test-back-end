c49a74f7e7a8b0e259354bb4b663d164898c401e
pranali95 <pranali@nimapinfotech.com>
2020-08-24 17:11:44 +0530

    server point changed to https

--- gold-admin-frontend/src/environments/environment.uat.ts
+++ gold-admin-frontend/src/environments/environment.uat.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     environmentName: 'uat',
-    apiUrl: 'http://emiapiuat.nimapinfotech.com/',
+    apiUrl: 'https://emiapiuat.nimapinfotech.com/',
     authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 
 };
