a6d325d6f9bd9e4122419d0519fdc2cbd28447d6
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-09-07 12:09:16 +0530

    changes in customer app kyc api

--- gold-loan-backend/routes/partPaymentCustomerApp.js
+++ gold-loan-backend/routes/partPaymentCustomerApp.js
@@ -6,14 +6,14 @@ const checkRolePermission = require('../middleware/checkRolesPermissions');
 const express = require('express');
 const route = express.Router();
 
-route.get('/view-log', checkAuth, checkRolePermission, wrapper(viewLog))
+route.get('/view-log', checkAuth, wrapper(viewLog))
 
-route.get('/part-payment-info', checkAuth, checkRolePermission, wrapper(getInterestInfo))
+route.get('/part-payment-info', checkAuth, wrapper(getInterestInfo))
 
-route.post('/check-part-amount', checkAuth, checkRolePermission, wrapper(checkPartAmount))
+route.post('/check-part-amount', checkAuth, wrapper(checkPartAmount))
 
-route.post('/confirm-payment-info', checkAuth, checkRolePermission, wrapper(payableAmountConfirmPartPayment))
+route.post('/confirm-payment-info', checkAuth, wrapper(payableAmountConfirmPartPayment))
 
-route.post('/payment', checkAuth, checkRolePermission, wrapper(partPayment))
+route.post('/payment', checkAuth, wrapper(partPayment))
 
 module.exports = route;   
