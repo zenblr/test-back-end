cc9cb50da8667230c414257ba5c01210acdee4f8
pranali95 <pranali@nimapinfotech.com>
2020-10-08 12:23:06 +0530

    env changed

--- gold-loan-backend/utils/razorpay.js
+++ gold-loan-backend/utils/razorpay.js
@@ -1,8 +1,8 @@
 let Razorpay = require('razorpay');
 
 const razorPayConfig = {
-    key_id: 'rzp_test_kH2RW0pO6ywG5C',
-    key_secret: 'N3j8rEFxrwmJwnxdubZxQXUU'
+    key_id: 'rzp_test_FvEtFgQ5WnMZ7J',
+    key_secret: '3Hhh6UQD9jZKLC1YxjKeO2IS'
 }
 
 let instance = new Razorpay(razorPayConfig);
