89577febeb2395ec641fe5434cea20ba0d6f51db
neebal <neebal@debian.debian>
2020-10-09 22:09:08 +0530

    Bug fixes

--- gold-admin-frontend/src/app/views/pages/admin/scrap-management/scrap-management.module.ts
+++ gold-admin-frontend/src/app/views/pages/admin/scrap-management/scrap-management.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'scrap-buying-calculator',
+        redirectTo: 'applied-scrap',
         pathMatch: 'full'
       },
       {
