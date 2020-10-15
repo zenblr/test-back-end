08ffc52aa72398dd16f833c1819f9ccaee244ed8
pranali95 <pranali@nimapinfotech.com>
2020-10-15 16:21:32 +0530

    redis host changed

--- gold-loan-backend/config/redis.js
+++ gold-loan-backend/config/redis.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: '6379',
     // HOST: '127.0.0.1',
 
-    HOST: 'redis'
+    HOST: 'augmont-loan-emi-uat-redis.d356pk.0001.aps1.cache.amazonaws.com'
         // git reset--hard
 
 }
