fd16d7d4e8f96ba5d3e1ee4e4f6ac473a8d7f95f
Bhushan Madaye <bhushanmadaye@nimapinfotech.com>
2020-10-19 16:07:27 +0530

    pkt-fe::alphabets directive correction allow special keys

--- gold-admin-frontend/src/app/core/_base/layout/directives/alphabets.directive.ts
+++ gold-admin-frontend/src/app/core/_base/layout/directives/alphabets.directive.ts
@@ -20,7 +20,7 @@ export class AlphabetsDirective {
   @HostListener('keydown', ['$event']) onKeydown(event: KeyboardEvent) {
     this.key = event.keyCode;
     // console.log(this.key);
-    if ((this.key >= 15 && this.key <= 64) || (this.key >= 123) || (this.key >= 96 && this.key <= 105)) {
+    if ((this.key >= 47 && this.key <= 64) || (this.key >= 123) || (this.key >= 96 && this.key <= 105)) {
       event.preventDefault();
     }
   }
