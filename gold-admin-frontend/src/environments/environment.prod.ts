b7218e832acb8bef93e50184cb90471de4a46c9f
pranali95 <pranali@nimapinfotech.com>
2020-10-19 21:09:20 +0530

    production url added

--- gold-admin-frontend/src/environments/environment.prod.ts
+++ gold-admin-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
-	apiUrl: 'http://13.234.229.92:3010/',
+	apiUrl: 'http://emi.augmont.com/',
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
--- gold-admin-frontend/src/environments/environment.uat.ts
+++ gold-admin-frontend/src/environments/environment.uat.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     environmentName: 'uat',
-    apiUrl: 'http://13.234.229.92:3010/',
+    apiUrl: 'http://emi.augmont.com/',
     authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 
 };
