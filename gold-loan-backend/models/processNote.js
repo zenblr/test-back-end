0fe3ea2202a0350baa656e388d3774542e0cf40f
Rupesh Ayare <rupesh@nimapinfotech.com>
2020-10-30 18:04:56 +0530

    process note complete

---
+++ gold-loan-backend/models/processNote.js
@@ -0,0 +1,46 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const ProcessNote = sequelize.define('processNote', {
+        // attributes
+        pdf: {
+            type: DataTypes.ARRAY(DataTypes.TEXT),
+            field: 'pdf'
+
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            field: 'user_id'
+        }
+    }, {
+        freezeTableName: true,
+        tableName: 'loan_process_note',
+    });
+
+    Banner.associate = function (models) {
+    }
+
+    Banner.prototype.toJSON = function () {
+        var values = Object.assign({}, this.get({ plain: true }));
+        let ProcessNotePdf = []
+        if (values.pdf) {
+            for (imgUrl of values.pdf) {
+                let URL = process.env.BASE_URL + imgUrl;
+                ProcessNotePdf.push(URL)
+            }
+        }
+        values.ProcessNotePdf = ProcessNotePdf
+        return values;
+    }
+
+    //Add_Banner
+    ProcessNote.addProcessNote = (pdf, userId) => ProcessNote.create({ pdf, userId });
+
+    //Update_ProcessNote
+    ProcessNote.updateProcessNote = (id, pdf, userId) => ProcessNote.update({ pdf, userId }, { where: { id } })
+
+    //Read_ProcessNote
+    ProcessNote.readProcessNote = () => ProcessNote.findAll();
+
+
+    return ProcessNote;
+}
