34522c0760b22eb9b8a9466591fecd8c1041a987
pranali95 <pranali@nimapinfotech.com>
2020-12-23 17:28:39 +0530

    Production fixed bugs 23-12-2020

--- gold-admin-frontend/src/environments/environment.prod.ts
+++ gold-admin-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
-	apiUrl: 'https://emi.augmont.com/',
+	apiUrl: 'https://emiapiuat.nimapinfotech.com/',
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
