5daa3448223f510b6867266cd643e60ed6361b8f
Ravi kumar <ravikumar@nimapinfotech.com>
2021-01-06 18:31:53 +0530

    partner include

--- gold-loan-backend/utils/loanFunction.js
+++ gold-loan-backend/utils/loanFunction.js
@@ -1286,13 +1286,19 @@ let getSingleLoanDetail = async (loanId, masterLoanId) => {
                         as: 'scheme'
                     },
                     {
+                        model: models.partner,
+                        as: 'partner',
+                        attributes: ['id', 'name']
+                    },
+                    {
                         model: models.customerLoan,
                         as: 'unsecuredLoan',
                         attributes: { exclude: ['createdAt', 'updatedAt', 'createdBy', 'modifiedBy', 'isActive'] },
                         include: [{
                             model: models.scheme,
                             as: 'scheme',
-                        }]
+                        },
+                        ]
                     }
                 ]
             },
