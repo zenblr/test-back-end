ebd0ecf632f7b9311743fbaafd04cb212a0fcbb6
dhanashri1 <dhanashri@nimapinfotech.com>
2021-01-15 20:06:52 +0530

    buy limit

--- gold-loan-backend/controllers/digitalGold/buy/buyProduct.js
+++ gold-loan-backend/controllers/digitalGold/buy/buyProduct.js
@@ -389,7 +389,7 @@ async function checkBuyLimit(id, totalAmount) {
 
     let newAndOldAmountTotal = Number(total) + Number(totalAmount);
 
-    if ((total > limit && customer.digiKycStatus == 'pending') || (newAndOldAmountTotal > limit && customer.digiKycStatus == 'pending')) {
+    if ((total > limit && customer.digiKycStatus == 'pending') || (newAndOldAmountTotal >= limit && customer.digiKycStatus == 'pending')) {
 
       const panno = customer.panCardNumber
 
@@ -418,7 +418,7 @@ async function checkBuyLimit(id, totalAmount) {
 
       }
 
-    } else if ((total > limit && customer.digiKycStatus == 'approved') || ((newAndOldAmountTotal > limit && customer.digiKycStatus == 'approved')) ) {
+    } else if ((total > limit && customer.digiKycStatus == 'approved') || ((newAndOldAmountTotal >= limit && customer.digiKycStatus == 'approved')) ) {
 
       return ({ message: "Your KYC is approved", success: true });
     } else if (totalAmount >= limit && customer.digiKycStatus == 'approved') {
@@ -437,7 +437,7 @@ async function checkBuyLimit(id, totalAmount) {
           return ({ message: "Your KYC is pending. Please complete KYC first", success: false });
 
         }
-      } else if(newAndOldAmountTotal > limit){
+      } else if(newAndOldAmountTotal >= limit){
 
         return ({ message: "Your KYC is pending. Please complete KYC first", success: false });
         
