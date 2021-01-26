f6ea47e94a043f36e46eabce6ed3218c92885f1b
Rupesh Ayare <rupesh@nimapinfotech.com>
2021-01-26 17:08:58 +0530

    change

--- gold-loan-backend/controllers/customer/customerMigration.js
+++ gold-loan-backend/controllers/customer/customerMigration.js
@@ -179,6 +179,7 @@ let createKyc = async (req) => {
                     let url;
                     let base64data;
                     let fullBase64Image;
+                    console.log(process.env.NODE_ENV)
                     if (process.env.NODE_ENV == "production" || process.env.NODE_ENV == "uat") {
                         url = process.env.BASE_URL + customer.panImage
                         const getAwsResp = await models.axios({
