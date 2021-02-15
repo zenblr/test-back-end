39df6b0cbf6ff021d646a90e8e87c6a32338958f
Pratik Bhayade <pratik@nimapinfotech.com>
2021-02-15 11:25:32 +0530

    Changes in environment files

--- gold-admin-frontend/src/environments/environment.prod.ts
+++ gold-admin-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
-	apiUrl: 'http://173.249.49.7:9220/',
+	apiUrl: 'https://emiapi.nimapinfotech.com/',
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
--- gold-admin-frontend/src/environments/environment.uat.ts
+++ gold-admin-frontend/src/environments/environment.uat.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     environmentName: 'uat',
-    apiUrl: 'http://173.249.49.7:9220/',
+    apiUrl: 'https://emiapi.nimapinfotech.com/',
     authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 
 };
