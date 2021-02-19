87a4625466bac1549ff99f28d8e4f366d3a2f255
Rupesh Ayare <rupesh@nimapinfotech.com>
2021-02-19 11:09:31 +0530

    add url in emi

--- gold-admin-frontend/src/environments/environment.ts
+++ gold-admin-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   isMockEnabled: true, // You have to switch this, when your real back-end is done
-  apiUrl: 'http://173.249.49.7:9220/',
+  apiUrl: 'https://emiapi1.nimapinfotech.com/',
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
   };
 
