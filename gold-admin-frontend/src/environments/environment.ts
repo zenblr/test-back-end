4ffca25adf236915530538558a96e2d8797418a3
Rupesh Ayare <rupesh@nimapinfotech.com>
2021-02-23 14:09:50 +0530

    changes in env file

--- gold-admin-frontend/src/environments/environment.ts
+++ gold-admin-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   isMockEnabled: true, // You have to switch this, when your real back-end is done
-  apiUrl: 'https://emiapi.nimapinfotech.com/',
+  apiUrl: 'https://emiapiuat.nimapinfotech.com/',
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
   };
 
