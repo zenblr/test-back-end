1745e8d1b113195e3cb4e690e70393b955a9badc
Ravi kumar <ravikumar@nimapinfotech.com>
2021-02-25 10:28:22 +0530

    skip url

--- gold-loan-backend/routes/appVersions.js
+++ gold-loan-backend/routes/appVersions.js
@@ -6,7 +6,7 @@ const { wrapper } = require('../utils/errorWrap');
 
 route.post('/', checkAuth, wrapper(addAppVersion));// add Identity Type
 
-route.get('/', checkAuth, wrapper(readAppVersion)); // read Identity Type
+route.get('/', wrapper(readAppVersion)); // read Identity Type
 
 module.exports = route;
 
