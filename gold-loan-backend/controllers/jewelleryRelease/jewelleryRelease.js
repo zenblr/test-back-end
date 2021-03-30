8c91988aedeeda44b743372e1d08decff3ec632a
Ravi kumar <ravikumar@nimapinfotech.com>
2021-03-30 17:31:35 +0530

    tottal amt updated

--- gold-loan-backend/controllers/jewelleryRelease/jewelleryRelease.js
+++ gold-loan-backend/controllers/jewelleryRelease/jewelleryRelease.js
@@ -205,8 +205,9 @@ exports.ornamentsAmountDetails = async (req, res, next) => {
 
         let interest = await nextDueDateInterest(loan.loan)
         let ornamentWeight = releaseData.ornamentWeight;
-        ornamentWeight.previousOutstandingAmount = Math.ceil(Number(ornamentWeight.previousOutstandingAmount)).toFixed(2)
+        // ornamentWeight.previousOutstandingAmount =   Math.ceil(Number(ornamentWeight.previousOutstandingAmount)).toFixed(2)
         let loanInfo = releaseData.loanInfo;
+        loanInfo.totalPayableAmount = Math.ceil(Number(ornamentWeight.previousOutstandingAmount)).toFixed(2)
         let amount = releaseData.amount;
         return res.status(200).json({ message: 'Success', ornamentWeight, loanInfo, amount, interest });
     } else {
